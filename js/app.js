3dc5e446d3bd7145f8f95f5fca1703f633a272ce
Tahfimul Latif <victory.josh.josh24@gmail.com>
2017-07-13 13:37:42 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,35 +1,13 @@
-var score=0;
+
 
 var choice1;
 
-var UC="";
+choice1=localStorage.getItem("Choice");
+console.log(choice1);
 
+computerChoice();
 
-function play(){
-//User picks their choice
-var UC=window.prompt("Enter a character. (Rock, Paper, or Scissors)");
-//Check for character spelling
-   if (UC === "Rock" || UC === "Paper" || UC === "Scissors" ){
-	  window.alert('You can type " Test " to view the fairness of computer choices.');
-	  choice1=UC;	 
-	  computerChoice();
- }else if (UC === "Test"){
-	
-	 test2();
-	 
- } else if (UC === ""){
-	 window.alert('You did not input anything.');
-	 play();
-	
- } else if (UC !== "Rock" || UC !== "Paper" || UC !== "Scissors" || UC !=="Test") {
-		 window.alert('You misplled the character name.');
-		 play();	
-		 
-		} 
-   
-};
-
-play();
+if (choice1==="Test"){test2();}
 
 function test2(){	   
 	   var i;	   
@@ -47,37 +25,35 @@ function test2(){
 		           PC="None";
 		           i--
 		           console.log("I0: "+i);
-		           N++;          
+		           N++          
 		     }else if ( PC1 >=1 &&  PC1 <= 3 ){
 	           PC="Rock";
 	           i++
 	           console.log("I1: "+i);
-	           R++;
+	           R++
 	         }else if(PC1 >=4 && PC1 <= 6){
 	           PC="Paper";
 	           i++
-	           console.log("I2: "+i);
-	          
-	           P++;
+	           console.log("I2: "+i);	          
+	           P++
 	         } else if (PC1 >=7 && PC1 <= 9) {
 	           PC="Scissors";
 	           i++
-	           console.log("I3: "+i);
-	           
-	           S++;
+	           console.log("I3: "+i);	           
+	           S++
 	         }  
 	         
 	     }
-	   
-	   document.getElementById("RESULT").innerHTML="Rock: "+R + "<br>"+ "Paper: "+P+"<br>"+"Scissors: "+S+"<br>"+"None: "+N;
-	   document.getElementById("Stat").innerHTML="Test Result";
-	   document.getElementById("vs").innerHTML="Ran: " + i + "x";
-	   document.getElementById("restart").innerHTML= "Press F5 or Restart button to restart.";
-	
-	   test();
-	  
+	     
+	        document.getElementById("RESULT").innerHTML="Rock: "+R + "<br>"+ "Paper: "+P+"<br>"+"Scissors: "+S+"<br>"+"None: "+N;
+		    document.getElementById("Stat").innerHTML="Test Result";
+		    document.getElementById("vs").innerHTML="Ran: " + i + "x";
+		    document.getElementById("restart").innerHTML= "Press F5 or Restart button to restart.";
+		    
+		    
+	       
+	     }
 
-	 }
 	 
 var choice2;
 
@@ -85,6 +61,7 @@ var PC;
 function computerChoice(){
 
 var PC1 = Math.floor(Math.random()*10);
+console.log(PC1);
 
 if (PC1 >=1 &&  PC1 <= 3){
 PC= "Rock";
@@ -106,88 +83,109 @@ choice2 = PC;
  choice1;
  
  choice2; 
-
+ var score;
  function compare(choice1, choice2){
 	
-	if (choice1 === choice2){
+	if (choice1 == choice2){
+		score=0;
+		console.log("Tie:"+score);		
+		return ('It is a tie! I am still better than you &#9757;');
 		
 		
-		return ('It is a tie!');
 		
 		
 	}else if (choice1 == "Rock"){
 		
 		if(choice2 == "Scissors"){
-			score++;
+			score=1;
 			
 			console.log(score);
 			
-			return ('You win!');
+			return ('You win! I will win another time &#9752;');	
+			
 			
 		}else{
-			score--;
+			score=-1;
 			
 			console.log(score);
 			
-			return ('Computer Wins¡');
+			return ('Computer Wins¡ Get on my LVL. I am the KING &#9889;');
 		}
 	}else if (choice1 == "Paper"){
 		if (choice2 == "Rock"){
-			score++;
+			score=1;
 			
 			console.log(score);
 			
-			return ('You win!');
+			return ('You win! I will win another time &#9752;');
 		}
 		else {
-			score--;
+			score=-1;
 			
 			console.log(score);
-			return ('Computer Wins¡');
+			return ('Computer Wins¡ Get on my LVL. I am the KING &#9889;');
 		}
 	}else if (choice1 == "Scissors"){
 		if (choice2 == "Rock"){
-			score--;
+			score=-1;
 		
 			console.log(score);
-			return ('Computer Wins¡');
+			return ('Computer Wins¡ Get on my LVL. I am the KING &#9889;');
 		}
 		else {
-			score++;
+			score=1;
 			
 			console.log(score);			
 					
-			return ('You win!');
+			return ('You win! I will win another time &#9752;');
 		}
-	} 
+	}
+	
 	 
 } 
+ 
+ window.onload= function(){
+	 	countDown=document.getElementById('Score');
+	 	console.log("score:"+ score);
+	  	document.getElementById("playerChoice").innerHTML="Your Choice: "+ "<br/>" +"<img src= " + "img/characters/"  + choice1 + ".png height=128px/>" +"<br/>" +choice1;
+	    document.getElementById("computerChoice").innerHTML="Computer Choice: " + "<br/>" + "<img src= " + "img/characters/"  + PC + ".png height=128px/>" + "<br/>"+ PC;
+	    document.getElementById("Result").innerHTML="The result is coming up...";
+	   
+	    countDown.innerHTML = "3",  
+	    
+	    setTimeout(function() {
+	        countDown.innerHTML = "2"+"<br>"+"Just wait..";
+	    }, 2000);    
+	   
+	     setTimeout(function() {
+	        countDown.innerHTML = "1"+"<br>"+"And now.";
+	    }, 4000);	
+	    
+	    document.getElementById("restart").innerHTML= "";
+	    document.getElementById("vs").innerHTML= "VS";
+	    document.getElementById("Stat").innerHTML="Game Stat"; 
+	    setTimeout(function (){		
+	        document.getElementById("playerChoice").innerHTML="";	     	
+	        document.getElementById("computerChoice").innerHTML="";
+	        document.getElementById("Result").innerHTML="Result: " + compare(choice1,choice2);
+	        document.getElementById("Score").innerHTML="Your gif:"+"<br/>"+"<img src=img/results/"+ score +".gif width=200px;>"+"<br/>"+"Your Score: " + score;
+	     	console.log("score:"+ score);
+	        document.getElementById("restart").innerHTML= "Press F5 or Restart button to restart.";
+	        document.getElementById("vs").innerHTML= "";    
+	    }, 5000); 
+	}
 
- window.onload= function (){
-    document.getElementById("playerChoice").innerHTML="Your Choice: "+ "<br/>" +"<img src= " + "img/characters/"  + choice1 + ".png height=128px/>" +"<br/>" +choice1;
-    document.getElementById("computerChoice").innerHTML="Computer Choice: " + "<br/>" + "<img src= " + "img/characters/"  + PC + ".png height=128px/>" + "<br/>"+ PC;
-    document.getElementById("Result").innerHTML="Result: " + compare(choice1,choice2);
-    document.getElementById("Score").innerHTML="Your Score: " + score;
-    document.getElementById("restart").innerHTML= "Press F5 or Restart button to restart.";
-    document.getElementById("vs").innerHTML= "VS";
-    document.getElementById("Stat").innerHTML="Game Stat";  	  
-    
-} 
+
  
- if (UC == "Test"){
- 
-	window.onload = function test(){		
-		    document.getElementById("playerChoice").innerHTML="";		   
-		    document.getElementById("computerChoice").innerHTML="";		 
-		    document.getElementById("Score").innerHTML="";
-		    
+ if (choice1 === "Test"){ 
+	 
+	 window.onload=function test() {
+	 
+	    document.getElementById("playerChoice").innerHTML="";		   
+	    document.getElementById("computerChoice").innerHTML="";		 
+	    document.getElementById("Score").innerHTML="";
+	    
+	 }
 	
 }
-	
-}
-
-
-
-
-		
-
+ 
